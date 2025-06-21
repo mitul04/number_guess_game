c516c680dd442b957623711e0972d24d5756c65b
mitul04 <mitulmanoj04@gmail.com>
2025-06-22 00:23:28 +0530

    desc in b1

--- number_guess_game.py
+++ number_guess_game.py
@@ -41,4 +41,7 @@ while choice.lower() == "yes":
 else:
     print("Thanks for playing!")
     quit()
-    
+
+# End of the number guessing game
+# This is a simple number guessing game where the user has to guess a randomly generated number within a specified range.
+# The game continues until the user decides to stop playing.
