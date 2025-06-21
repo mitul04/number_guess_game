b1b5cf18b53b43571808df8ab8e8e52a24e1369b
mitul04 <mitulmanoj04@gmail.com>
2025-06-21 21:41:58 +0530

    Project in Python code

---
+++ number_guess_game.py
@@ -0,0 +1,43 @@
+import random
+
+choice = "yes"
+
+while choice.lower() == "yes":
+    start, stop = input("Give me a range of numbers: ").split()
+
+    if start.isdigit() and stop.isdigit():
+        start, stop = map(int, (start, stop))
+
+    else:
+        print("Both should be numbers.")
+        quit()
+
+    number = random.randint(start, stop)
+    guesses = 0
+
+    print("Let's see in how many guesses you can guess this number")
+
+    while True:
+        guesses += 1
+        guess = input(f"Guess {guesses}: ")
+
+        if not guess.isdigit():
+            print("Please enter a valid number.")
+            continue
+        guess = int(guess)
+
+        if guess == number:
+            print("You got it right in", guesses, "guesses")
+            break
+
+        elif guess > number:
+            print("You are ahead of the number")
+
+        else:
+            print("You are behind the number")
+
+    choice = input("Play again? (yes/no) ")
+
+else:
+    print("Thanks for playing!")
+    quit()
